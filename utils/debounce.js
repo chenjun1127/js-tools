552c5dacd5d3213f98f26e8d7fbd7a3cc20f1aa7
C~JUN <kiss19861127@126.com>
2023-07-27 10:05:13 +0800

    Update debounce.js

--- utils/debounce.js
+++ utils/debounce.js
@@ -1,3 +1,4 @@
+// ...args是一个使用扩展语法（Spread Syntax）的语法，它表示一个参数数组。
 function debounce(fnc, delay) {
   let timerId;
   return function (...args) {
@@ -7,3 +8,13 @@ function debounce(fnc, delay) {
     }, delay);
   };
 }
+/*
+ * 使用
+function handleResize() {
+  console.log('Window resized');
+}
+
+const debouncedResize = debounce(handleResize, 200);
+
+window.addEventListener('resize', debouncedResize);
+*/
